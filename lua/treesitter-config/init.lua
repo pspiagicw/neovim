2bd9026141a076ae0af171936dba0881ce31cc39
pspiagicw <pspiagicw@gmail.com>
2025-05-19 05:52:34 +0530

    feat: updated plugins, added tressiter-ensure-installed

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -7,34 +7,52 @@
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
 parser_config.tablegen = {
-	install_info = {
-		url = "https://github.com/pspiagicw/tree-sitter-tablegen",
-		files = { "src/parser.c", "src/scanner.c" },
-	},
-	filetype = "tablegen",
+    install_info = {
+        url = "https://github.com/pspiagicw/tree-sitter-tablegen",
+        files = { "src/parser.c", "src/scanner.c" },
+    },
+    filetype = "tablegen",
 }
 
 parser_config.fener = {
-	install_info = {
-		url = "~/code/other-projects/tree-sitter-fener",
-		files = { "src/parser.c" },
-	},
+    install_info = {
+        url = "~/code/other-projects/tree-sitter-fener",
+        files = { "src/parser.c" },
+    },
 }
 parser_config.hotshot = {
-	install_info = {
-		url = "~/code/other-projects/tree-sitter-hotshot",
-		files = { "src/parser.c" },
-	},
-	filetype = "ht",
+    install_info = {
+        url = "~/code/other-projects/tree-sitter-hotshot",
+        files = { "src/parser.c" },
+    },
+    filetype = "ht",
 }
 vim.filetype.add({ extension = { fn = "fener", ht = "hotshot" } })
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "c", "vimdoc", "lua", "query" },
-	indent = {
-		enable = false,
-	},
-	highlight = {
-		enable = true,
-	},
+    ensure_installed = {
+        "go",
+        "c",
+        "vimdoc",
+        "lua",
+        "query",
+        "rust",
+        "jsonc",
+        "markdown",
+        "markdown_inline",
+        "cpp",
+        "css",
+        "bash",
+        "editorconfig",
+        "gomod",
+        "gosum",
+        "meson",
+        "ninja"
+    },
+    indent = {
+        enable = false,
+    },
+    highlight = {
+        enable = true,
+    },
 })
